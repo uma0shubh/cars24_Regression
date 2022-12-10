5de9de4ed0632029d17b9ff4b5e6a39f14945330
Uma <97823191+uma0shubh@users.noreply.github.com>
2022-12-10 19:12:03 +0530

    Update car24_analysis_20221210.py

--- car24_analysis_20221210.py
+++ car24_analysis_20221210.py
@@ -550,10 +550,10 @@ with Graphical:
         x = df1["registrationstate"]
     
     #fig303 = plt.subplot()
-    #fig303 = x.value_counts().plot(kind='bar', figsize=(15,4))
-    #plt.xticks(rotation=90)
-    #plt.show()
-    st.write(x.value_counts().plot(kind='bar', figsize=(15,4)))
+    fig303 = x.value_counts().plot(kind='bar', figsize=(15,4))
+    plt.xticks(rotation=90)
+    plt.show()
+    st.pyplot(fig303)
     
     # ************************************************************* 
     """# Bivariate plots """
