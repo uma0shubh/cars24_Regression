1e50b3f4c0fb5908d39cd1e01baf5ac300a3b268
Uma <97823191+uma0shubh@users.noreply.github.com>
2022-12-12 19:46:58 +0530

    Update car24_analysis_20221210.py

--- car24_analysis_20221210.py
+++ car24_analysis_20221210.py
@@ -647,11 +647,15 @@ with Graphical:
         x2 = df1["bodytype"]
     elif x_axis_2 == "RegistrationState":
         x2 = df1["registrationstate"]
+        
+    fig303 = df1.groupby('city').size().plot(kind='bar')
     
-    fig303 = plt.figure(figsize = (15,4))
-    #plt.bar(x.value_counts())
-    plt.bar(x, df1["price"])
-    plt.xticks(rotation=90)
+#     fig303 = plt.figure(figsize = (15,4))
+#     #plt.bar(x.value_counts())
+#     plt.bar(x, df1["price"])
+#     plt.xticks(rotation=90)
+
+#     fig303 = df1.plot.bar(x=, rot=0)
     st.write(fig303)
     
 #     fig303 = sns.set(font_scale=1.4)
